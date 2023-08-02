3ee91dcaa52cb31ae2d52c4fc751a90ebbfb533e
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-08-02 06:34:05 -0600

    Set up basic files for project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log('index.js');
+
+//title, description, deDate, priority
+//Projects/separate lists of to-dos
+//Create new to-dos
+//Set to-dos as complete
+//change to-dos priority
+
+//View all projects
+//View all to-dos in each project
+//View to-do details
+//Delete a to-do
