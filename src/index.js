ecb46d0fff6e4404e84df477c6ab18c28b74bacf
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-08-05 06:30:36 -0600

    Reset fields on btn submits

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const getProjects = () => {
 }
 
 const createProject = (name) => {
+  // console.log (name);
+  return {name};
 }
 
 const displayProjects = ()  => {
@@ -28,6 +30,11 @@ const createTodo = (() => {
   const projectBtn = document.querySelector('.add-project');
   const formMessage = document.querySelector('#form-message');
   const formContainer = document.querySelector('.form-container');
+  const date = document.querySelector('.date');
+  const priority = document.querySelector('.priority');
+  const project = document.querySelector('.project-select');
+  const projectSubmitBtn = document.querySelector('.project-btn');
+  const todoSubmitBtn = document.querySelector('.todo-btn');
 
   const showForm = (msg) => {
     todoBtn.classList.add('hidden');
@@ -43,33 +50,66 @@ const createTodo = (() => {
     formContainer.classList.add('hidden');
   }
 
+  //add todo button show form
   todoBtn.addEventListener('click', () => {
     showForm('Add To-do');
-    document.querySelector('.date').classList.remove('hidden');
-    document.querySelector('.priority').classList.remove('hidden');
-    document.querySelector('.project-select').classList.remove('hidden');
+    date.classList.remove('hidden');
+    priority.classList.remove('hidden');
+    project.classList.remove('hidden');
+    todoSubmitBtn.classList.remove('hidden');
+    projectSubmitBtn.classList.add('hidden');
   });
 
+  //submit todo form and clear fields 
+
+
+  //create project button show form
   projectBtn.addEventListener('click', () => {
     showForm('Create Project');
-    document.querySelector('.date').classList.add('hidden');
-    document.querySelector('.priority').classList.add('hidden');
-    document.querySelector('.project-select').classList.add('hidden');
+    date.classList.add('hidden');
+    priority.classList.add('hidden');
+    project.classList.add('hidden');
+    todoSubmitBtn.classList.add('hidden');
+    projectSubmitBtn.classList.remove('hidden');
   })
 
-  const submitBtn = document.querySelector('.submit');
-  submitBtn.addEventListener('click', (e) => {
+  //submit todo form and clear fields
+  todoSubmitBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    hideForm()
-;    console.log('hey')
+    hideForm();
+    //reset inputs
+    let inputs = document.querySelectorAll('input');
+    for (i = 0; i < inputs.length; i++) {
+      inputs[i].value = "";
+    }
+
+    //reset priority
+    priority.selectedIndex = 0;
+    document.querySelector('#project').selectIndex = 0;
+  })
+
+  //submit project form and clear fields
+  projectSubmitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideForm();
+    //reset inputs
+    let inputs = document.querySelectorAll('input');
+    for (i = 0; i < inputs.length; i++) {
+      inputs[i].value = "";
+    }
+    createProject(document.querySelector('#name').value);
   })
 
 })();
 
-const projectSelect = document.getElementById('project');
-const projectOption = document.createElement('option');
-projectOption.setAttribute('value', 'Default');
-projectOption.textContent = "Default";
 
-projectSelect.appendChild(projectOption);
+const storeProjectName = () => {
+  const projectSelect = document.getElementById('project');
+  const projectOption = document.createElement('option');
+  projectOption.setAttribute('value', 'Default');
+  projectOption.textContent = "Default";
+  
+  projectSelect.appendChild(projectOption);
+}
+
 
