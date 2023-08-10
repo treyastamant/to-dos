a882ee7eb73cdda062c3efb515eb385ad62cf6ff
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-08-10 05:34:46 -0600

    Store project names in local storage

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,11 @@
 import { btnActions } from "./btn-actions";
 
 export const projectStuff = (() => {
-  const projectList = [];
+  // localStorage.clear();
+  const projectList = JSON.parse(localStorage.getItem("projectList")) || ['Personal'];
+  
+  console.log(projectList)
+  
   
   //add project to drop-down
   const populateProjectDropDown = (name, type, index) => {
@@ -22,31 +26,46 @@ export const projectStuff = (() => {
 
   //show project name on page
   const displayProject = (name, type, index) => {
-    if (type === "new") {
-      const projectContainer = document.querySelector('.project-container');
+    const projectContainer = document.querySelector('.project-container');
+    projectContainer.innerHTML = "";
+    projectList.forEach((e) => {
       const projectName = document.createElement('h2');
       projectName.classList.add('project-name');
       const editIcon = document.createElement('span');
       editIcon.classList.add ('material-symbols-rounded', 'edit');
       editIcon.textContent = 'edit';
       editIcon.addEventListener('click', () => {
-        editAction(`Edit Project Name`, 'edit project', name)
+        editAction(`Edit Project Name`, 'edit project', e)
       });
-      projectName.textContent = name;
+      projectName.textContent = e;
       projectContainer.appendChild(projectName);
       projectName.appendChild(editIcon);
-    }
-    if (type === "edit") {
-      const projectName = document.querySelectorAll('.project-name');
-      projectName[index].textContent = name;
-      const editIcon = document.createElement('span');
-      editIcon.classList.add ('material-symbols-rounded', 'edit');
-      editIcon.textContent = 'edit';
-      editIcon.addEventListener('click', () => {
-        editAction(`Edit Project Name`, 'edit project', name)
-      });
-      projectName[index].appendChild(editIcon);
-    }
+    })
+
+    // if (type === "new") {
+    //   const projectName = document.createElement('h2');
+    //   projectName.classList.add('project-name');
+    //   const editIcon = document.createElement('span');
+    //   editIcon.classList.add ('material-symbols-rounded', 'edit');
+    //   editIcon.textContent = 'edit';
+    //   editIcon.addEventListener('click', () => {
+    //     editAction(`Edit Project Name`, 'edit project', name)
+    //   });
+    //   projectName.textContent = name;
+    //   projectContainer.appendChild(projectName);
+    //   projectName.appendChild(editIcon);
+    // }
+    // if (type === "edit") {
+    //   const projectName = document.querySelectorAll('.project-name');
+    //   projectName[index].textContent = name;
+    //   const editIcon = document.createElement('span');
+    //   editIcon.classList.add ('material-symbols-rounded', 'edit');
+    //   editIcon.textContent = 'edit';
+    //   editIcon.addEventListener('click', () => {
+    //     editAction(`Edit Project Name`, 'edit project', name)
+    //   });
+    //   projectName[index].appendChild(editIcon);
+    // }
   }
 
   const editAction = (msg, type, name) => {
@@ -63,20 +82,27 @@ export const projectStuff = (() => {
   //project factory function
   const createProject = (name) => {
     projectList.push(name);
+    localStorage.setItem("projectList", JSON.stringify(projectList));
+
     populateProjectDropDown(name, 'new');
     displayProject(name, 'new');
+
   }
 
   const updateProject = (currentName, newName) => {
     console.log(currentName, newName);
     let index = projectList.indexOf(currentName);
     projectList[index] = newName;
+    localStorage.setItem("projectList", JSON.stringify(projectList));
     displayProject(newName, 'edit', index);
     populateProjectDropDown(newName, 'edit', index);
   }
 
-  //create default project
-  createProject('Personal');
-
+  // if (localStorage.getItem("projectList")) {
+    // projectList.map((projectList) => {
+      displayProject(projectList);
+    // });
+  // } 
+  
   return {createProject, projectList, updateProject}
 })();
